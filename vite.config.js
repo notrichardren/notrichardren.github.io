10691a8b564f391e7d52c518bdb913f02e48baa0
Richard Ren <34405553+notrichardren@users.noreply.github.com>
2024-12-23 23:11:20 -0500

    debugginggg

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/notrichardren.github.io/',  // or '/' for local development
+  base: '/',  // or '/' for local development
   plugins: [react()],
 })
